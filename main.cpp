2263638a572599d9476c6f5a67344675c085c011
G Roques <groques360@gmail.com>
2018-03-10 18:04:05 -0600

    Open file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 std::string get_filename(int argc, char** argv);
+std::ifstream open_file(std::string filename);
 
 
 int main(int argc, char** argv) 
 {
     std::string filename = get_filename(argc, argv);
-    std::cout << filename << std::endl;
+
+    std::ifstream file = open_file(filename);
+
+    file.close();
+
     return 0;
 }
 
@@ -19,4 +25,14 @@ std::string get_filename(int argc, char** argv)
     }
     std::string filename(argv[1]);
     return filename;
+}
+
+std::ifstream open_file(std::string filename)
+{
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Error: Cannot open file '" << filename << "'.\n";
+        exit(1);
+    }
+    return file;
 }
