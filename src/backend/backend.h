b7a7d32f5d7c48c2624fd5f25b9ab57114ae4273
G Roques <groques360@gmail.com>
2018-04-24 10:39:32 -0500

    Move semantic analyzer code to backend

--- src/backend/backend.h
+++ src/backend/backend.h
@@ -2,7 +2,7 @@
 #define BACKEND_H
 
 #include "../parser/parser.h"
-#include "semantic_analyzer/semantic_analyzer.h"
+#include "var_stack/var_stack.h"
 
 /**
  * Backend part of compiler responsible for:
@@ -13,10 +13,17 @@
 class Backend
 {
     public:
-        void traverse(Node* parse_tree);
-    
+        void traverse(Node* node);
+
     private:
-        SemanticAnalyzer semantic_analyzer;
+        VarStack var_stack;
+        static std::set<std::string> new_scope_labels;
+        void check_for_variables(Node* node);
+        void check_location(int location, std::string err_msg, Token token);
+        void print_error_and_exit(std::string msg, Token token);
+        void traverse_children(Node* node);
+        static bool introduces_new_scope(Node* node);
+        static bool contains_variable_declarations(Node* node);
 };
 
 #endif
